304ea11a6938927cdf570e861d6853abe8cafdb7
Mohd Nazarul Fitri <mohd-nazarul-fitri_mohd-azman@astro.com.my>
2023-04-21 21:46:24 +0800

    add env

--- services/firebaseApp.tsx
+++ services/firebaseApp.tsx
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDw7H-zIt5UMFRuSJYOs4Wv9aXu4KUhKPU",
-  authDomain: "lumie-6cb4c.firebaseapp.com",
-  projectId: "lumie-6cb4c",
-  storageBucket: "lumie-6cb4c.appspot.com",
-  messagingSenderId: "770513594285",
-  appId: "1:770513594285:web:88a5e4eb12d6e00d8c2324",
-  measurementId: "G-H9R8P5MHMG",
+  apiKey: process.env.NEXT_PUBLIC_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
