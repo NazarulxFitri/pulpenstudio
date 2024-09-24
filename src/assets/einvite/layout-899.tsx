95d433a2cf29f94a15e8937d6e3cde165e4262df
Nazarul <nazarul@borong.com>
2024-09-24 18:58:41 +0800

    Update

---
+++ src/assets/einvite/layout-899.tsx
@@ -0,0 +1,268 @@
+import { Box, Container, Grid, List, Paper, styled } from "@mui/material";
+import {
+  Allura,
+  Cormorant_SC,
+  Lora,
+  Oooh_Baby,
+  Playfair_Display,
+  Ubuntu,
+} from "next/font/google";
+import useGetEinvite from "@/data/useGetEinvite";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import useGetCountDownTimer from "@/data/useGetCountDownTimer";
+import Widget from "@/components/Widget";
+import ReactPlayer from "react-player";
+import { CommentList, Door } from "@/components";
+import Image from "next/image";
+import { locale } from "@/utils/Locale";
+
+const ubuntu = Ubuntu({ subsets: ["latin"], weight: "700" });
+const playfair = Playfair_Display({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
+const allura = Allura({ subsets: ["latin"], weight: ["400"] });
+const cormorant = Cormorant_SC({
+  subsets: ["latin"],
+  weight: ["300", "400", "700"],
+});
+const lora = Lora({ subsets: ["latin"], weight: ["400", "700"] });
+
+interface DateTimeConfig {
+  countdownTimer: {
+    d: number;
+    h: number;
+    m: number;
+    s: number;
+  };
+}
+
+const Special = styled("p")(() => ({
+  fontFamily: `${allura.style.fontFamily} !important` || "auto",
+  fontSize: "24px",
+  textAlign: "center",
+}));
+
+const Title = styled("h1")(() => ({
+  color: "#a96b4e",
+  lineHeight: "0.75em",
+  fontFamily: `${cormorant.style.fontFamily} !important` || "auto",
+  fontSize: "36px",
+  textAlign: "center",
+  fontWeight: "lighter",
+}));
+
+const SubTitle = styled("p")(() => ({
+  fontSize: "14px",
+  fontWeight: "300",
+  fontFamily: `${cormorant.style.fontFamily} !important` || "auto",
+  textAlign: "center",
+  letterSpacing: "0.25em",
+  textTransform: "uppercase",
+}));
+
+const Text = styled("p")(() => ({
+  fontFamily: `${lora.style.fontFamily} !important` || "auto",
+  fontSize: "16px",
+  textAlign: "center",
+}));
+
+const MiniText = styled("p")(() => ({
+  fontFamily: `${playfair.style.fontFamily} !important` || "auto",
+  fontSize: "14px",
+  textAlign: "center",
+  letterSpacing: "0.1em",
+}));
+
+const UbuntuText = styled("p")(() => ({
+  fontFamily: `${ubuntu.style.fontFamily} !important` || "auto",
+  fontWeight: "700",
+  fontSize: "16px",
+  textAlign: "center",
+}));
+
+const Layout899: React.FC = () => {
+  const router = useRouter();
+  const eInviteId = router.query.eInviteId;
+  const [countdownTimer, setCountdownTimer] = useState<DateTimeConfig>();
+  const { data } = useGetEinvite(eInviteId as string);
+  const item = data?.data;
+  const listComments = data?.comments && [...data?.comments!].reverse();
+  const musicUrl = item?.musicUrl;
+  const dateJs = new Date(item?.dateTime!);
+  const fullDate = dateJs?.toLocaleString("ms-MY", {
+    day: "numeric",
+    month: "numeric",
+    year: "numeric",
+  });
+  const timeStart = dateJs.toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  });
+
+  const dayRaw = dateJs.getDay();
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const dayText = daysOfWeek[dayRaw];
+
+  const monthRaw = dateJs.getMonth();
+  const monthsList = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const monthText = monthsList[monthRaw];
+  const dateText = dateJs.getDate();
+  const yearText = dateJs.getFullYear();
+
+  const currentDate = new Date().getTime();
+  const selectedDate = dateJs.getTime();
+  const isCounting = currentDate < selectedDate;
+  const countdownTimerParam: DateTimeConfig = useGetCountDownTimer(
+    item?.dateTime
+  );
+  const [musicStart, setMusicStart] = useState(false);
+
+  const [clickOpen, setClickOpen] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountdownTimer(countdownTimerParam);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [countdownTimerParam]);
+
+  return (
+    <Box
+      maxWidth="400px"
+      sx={{
+        background: "#fff9ea",
+        boxShadow: "1px 1px 10px #fff9ea",
+        m: "auto",
+        position: "relative",
+        overflow: "hidden",
+        height: clickOpen ? "100%" : "100vh",
+      }}
+      onTouchStart={() => setMusicStart(true)}
+      onClick={() => setMusicStart(true)}
+    >
+      <Door color="transparent" {...{ clickOpen, setClickOpen }} yasminFaiz>
+        <Text
+          sx={{ fontSize: "10px", pt: 1, mb: -1 }}
+          dangerouslySetInnerHTML={{
+            __html: "Sila klik",
+          }}
+        />
+      </Door>
+      <Box sx={{ visibility: "hidden", position: "absolute" }}>
+        <ReactPlayer
+          url={musicUrl}
+          playing={musicStart}
+          loop={true}
+          width="1%"
+          height="1%"
+          controls={true}
+        />
+      </Box>
+      <Box
+        sx={{
+          height: "100vh",
+          position: "relative",
+        }}
+      >
+        <Box>
+          <Image
+            src="/media/animation/yasmin-faiz.svg"
+            alt="Yasmin Faiz"
+            width={400}
+            height={800}
+            style={{ display: "block" }}
+          />
+        </Box>
+      </Box>
+
+      <Image
+        src="/media/animation/content-yasmin-faiz.svg"
+        alt="Yasmin Faiz"
+        width={400}
+        height={800}
+        style={{ display: "block" }}
+      />
+      <Container>
+        <Box
+          id="comment"
+          mb={2}
+          sx={{
+            px: 2,
+            pb: 10,
+          }}
+        >
+          <Text
+            dangerouslySetInnerHTML={{
+              __html: "<b>Ucapan</b> | <i>Wishes</i>",
+            }}
+            sx={{ pb: 2, fontSize: "16px" }}
+          />
+          <Grid container>
+            <Grid item xs={12}>
+              <Paper
+                sx={{
+                  background: "#fff",
+                  boxShadow: "unset",
+                  maxHeight: "400px",
+                  overflow: "scroll",
+                }}
+              >
+                <List sx={{ p: 0, mb: 4 }}>
+                  {/* @ts-ignore */}
+                  {listComments?.map((comment: any, idx: string) => (
+                    <CommentList
+                      {...{ comment, idx }}
+                      key={idx}
+                      bgColor="#fff"
+                      textColor="#131313"
+                    />
+                  ))}
+                </List>
+              </Paper>
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
+      <Box>
+        <Widget
+          language={item?.language!}
+          iconColor="#131313"
+          color="#fff9ea"
+          location={{ text: item?.location!, mapUrl: item?.mapUrl! }}
+          contact={{
+            number1: item?.phonePerson1!,
+            number2: item?.phonePerson2!,
+            name1: item?.namePerson1!,
+            name2: item?.namePerson2!,
+          }}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default Layout899;
--- src/components/Door/index.tsx
+++ src/components/Door/index.tsx
@@ -13,6 +13,7 @@ interface DoorProps {
   khairuAmira?: boolean;
   aisyahEffi?: boolean;
   asyiqinasyraf?: boolean;
+  yasminFaiz?: boolean;
 }
 
 const Door: React.FC<DoorProps> = ({
@@ -27,7 +28,76 @@ const Door: React.FC<DoorProps> = ({
   khairuAmira,
   aisyahEffi,
   asyiqinasyraf,
+  yasminFaiz,
 }) => {
+  if (yasminFaiz)
+    return (
+      <Box sx={{ position: "absolute", width: "100%", height: "100%" }}>
+        <Box
+          sx={{
+            display: "flex",
+            position: "relative",
+          }}
+        >
+          <Box
+            className={
+              clickOpen ? "animate__animated animate__slideOutLeft" : ""
+            }
+            id="left-door"
+            sx={{
+              background: specialBg ? "" : "#FFF",
+              backgroundImage: specialBg
+                ? "url('/media/animation/test12.PNG')"
+                : "",
+              backgroundSize: "contain",
+              backgroundRepeat: "round",
+              width: "50%",
+              height: "100vh",
+              zIndex: "2",
+            }}
+          />
+          <Box
+            className={
+              clickOpen ? "animate__animated animate__slideOutRight" : ""
+            }
+            id="right-door"
+            sx={{
+              backgroundImage: "url('/media/animation/slice.svg')",
+              backgroundSize: "cover",
+              boxShadow: specialBg ? "unset" : "-10px 0px 20px #D0D0D0",
+              width: "50%",
+              height: "100vh",
+              zIndex: "2",
+            }}
+          />
+          <Box
+            className={clickOpen ? "animate__animated animate__fadeOut" : ""}
+            onClick={() => setClickOpen(true)}
+            sx={{
+              background: "#FFF",
+              borderRadius: specialBg ? "0" : "80%",
+              boxShadow: specialBg ? "0" : `-8px 8px 8px #D0D0D0`,
+              px: removePadding ? 0 : 3,
+              pt: removePadding ? 0 : 4,
+              pb: specialBg ? 6 : removePadding ? 0 : 4,
+              position: "absolute",
+              textAlign: "center",
+              left: "50%",
+              top: {
+                xs: specialBg ? "50%" : "20%",
+                sm: specialBg ? "50%" : "50%",
+              },
+              transform: "translate(-50%,-50%)",
+              minWidth: "60px",
+              zIndex: "3",
+            }}
+          >
+            {children}
+          </Box>
+        </Box>
+      </Box>
+    );
+
   if (asyiqinasyraf)
     return (
       <Box sx={{ position: "absolute", width: "100%", height: "100%" }}>
