287c2f4dca93d7215cd3b0b426ab137a4cabe9ca
Izzati Rafi <nazarulxfitri@gmail.com>
2025-07-12 23:11:42 +0800

    update document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,34 +12,28 @@ export default function Document() {
           href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
         />
 
-        <meta name="twitter:card" content="summary" />
-        <meta
-          name="twitter:title"
-          content="Pulpen Studio - Digital Invitation Card"
-        />
-        <meta
-          name="twitter:description"
-          content="Digital Invitation Card builder high quality & affordable price"
-        />
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Qayyum & Amirah's Wedding Invitation" />
+        <meta name="twitter:description" content="Join us to celebrate the wedding of Qayyum & Amirah." />
         <meta
           name="twitter:image"
-          content="https://www.pulpenstudio.com/_next/image?url=%2Fmedia%2Fgeneral%2Fpulpen_logo.png&w=384&q=75"
+          content="https://www.pulpenstudio.com/media/qayyum-amirah/qayyum_amira.jpeg"
         />
 
+        {/* Open Graph Meta Tags */}
         <meta property="og:type" content="website" />
         <meta property="og:locale" content="en_CA" />
-        <meta
-          property="og:title"
-          content="Pulpen Studio - Digital Invitation Card"
-        />
+        <meta property="og:site_name" content="Pulpen Studio" />
+        <meta property="og:title" content="Qayyum & Amirah's Wedding Invitation" />
+        <meta property="og:description" content="Join us to celebrate the wedding of Qayyum & Amirah." />
         <meta
           property="og:image"
-          content="https://www.pulpenstudio.com/_next/image?url=%2Fmedia%2Fgeneral%2Fpulpen_logo.png&w=384&q=75"
+          content="https://www.pulpenstudio.com/media/qayyum-amirah/qayyum_amira.jpeg"
         />
-        <meta property="og:site_name" content="Name of your website" />
         <meta
-          property="og:description"
-          content="Digital Invitation Card builder high quality & affordable price"
+          property="og:url"
+          content="https://www.pulpenstudio.com/e-invite/live/wedding-of-qayyum-and-amirah"
         />
       </Head>
 
